58b5f824ffdf997e626589fe2527ee2b6a6d0fda
paullo97 <paulo.cesar97@hotmail.com>
2022-08-27 14:55:36 -0300

    create components to list and card of usage in list

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Article } from 'src/app/core/services/models/response.model';
+
+@Component({
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: [ './list.component.scss' ]
+})
+export class ListComponent
+{
+    @Input()
+    public list: Array<Article> = [];
+}
