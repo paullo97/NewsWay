48a103cbd1ce0bf455d8dd6805c65d578b17e9b8
paullo97 <paulo.cesar97@hotmail.com>
2022-08-27 13:42:39 -0300

    include architeture to core and services

---
+++ src/app/core/services/services.module.ts
@@ -0,0 +1,13 @@
+
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+    imports: [
+        HttpClientModule
+    ],
+    providers: []
+})
+export class ServicesModule
+{ }
