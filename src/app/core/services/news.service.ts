09f86b4cfa4f068d800ce6060276d57c8a030d71
paullo97 <paulo.cesar97@hotmail.com>
2022-08-30 09:57:26 -0300

    include service do get languages mock

--- src/app/core/services/news.service.ts
+++ src/app/core/services/news.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { delay, Observable, of } from 'rxjs';
 import { api, ApiKey } from 'src/environments/constants';
-import { RequestModel, Response } from './models/response.model';
+import { languagesModel } from './models/language.model';
+import { RequestModel } from './models/request.model';
+import { Response } from './models/response.model';
 
 @Injectable()
 export class NewsService
@@ -12,24 +14,17 @@ export class NewsService
     )
     { }
 
+    /**
+     * It's a function that returns an Observable of type Response, which is a type that I created to
+     * represent the response of the API
+     * @param {RequestModel} [request] - RequestModel
+     * @returns The response is a JSON object with the following structure:
+     */
     public getNews(request?: RequestModel): Observable<Response>
     {
-        console.log('Service');
-        console.log({
-            apiKey: ApiKey,
-            q: encodeURI(request?.q || ''),
-            pageSize: 100,
-            language: request?.language || 'pt',
-            ...(!!request?.searchIn && { searchIn: request.searchIn}),
-            ...(!!request?.from && { from: request.from }),
-            ...(!!request?.to && { to: request.to }),
-            ...(!!request?.sortBy && { sortBy: request.sortBy})
-        });
-    //   encodeURI() Use in paramether q
-        // return this.http.get<Response>(`https://newsapi.org/v2/everything?q='Apple'&from=2022-08-27&sortBy=popularity&language=pt&apiKey=0568ee4a0aad482088d941dc5347d0a7&pageSize=10`)
         return this.http.get<Response>(api.system.get({
             apiKey: ApiKey,
-            q: encodeURI(request?.q || ''),
+            q: request?.q || '',
             pageSize: 100,
             language: request?.language || 'pt',
             ...(!!request?.searchIn && { searchIn: request.searchIn}),
@@ -39,5 +34,24 @@ export class NewsService
             ...(!!request?.domains && { domains: request.domains }),
             ...(!!request?.excludeDomains && { excludeDomains: request.excludeDomains })
         }))
+    }
+
+    public getLanguages(): Observable<Array<languagesModel>>
+    {
+        return of([
+            { value: 'ar', viewValue: 'Arabic' },
+            { value: 'de', viewValue: 'German' },
+            { value: 'en', viewValue: 'English' },
+            { value: 'es', viewValue: 'Spanish' },
+            { value: 'fr', viewValue: 'French' },
+            { value: 'he', viewValue: 'Hebrew' },
+            { value: 'it', viewValue: 'Italian' },
+            { value: 'nl', viewValue: 'Dutch' },
+            { value: 'no', viewValue: 'Norwegian' },
+            { value: 'pt', viewValue: 'Portuguese' },
+            { value: 'ru', viewValue: 'Russian' },
+            { value: 'sv', viewValue: 'Swedish' },
+            { value: 'zh', viewValue: 'Chinese' }
+        ]).pipe(delay(1500));
     }
 }
