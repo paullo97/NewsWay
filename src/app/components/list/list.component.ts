46c7f1317896a4c39021d9a782af2336ab45540e
paullo97 <paulo.cesar97@hotmail.com>
2022-08-30 02:15:07 -0300

    include methods to resize height

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,27 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Article } from 'src/app/core/services/models/response.model';
 import { DialogComponentComponent } from '../dialog-component/dialog-component.component';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: [ './list.component.scss' ]
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: [ './list.component.scss' ]
 })
-export class ListComponent
+export class ListComponent implements OnInit
 {
-  @Input()
-  public list: Array<Article> = [];
+    @Input()
+    public list: Array<Article> = [];
 
-  constructor(private dialog: MatDialog)
-  { }
+    public innerHeight: string = '';
 
-  public openDialog(item: Article): void
-  {
-    this.dialog.open(DialogComponentComponent, {
-      data: {
-        article: item
-      }
-    });
-  }
+    constructor(private dialog: MatDialog)
+    { }
+
+    public ngOnInit(): void
+    {
+        this.refreshHeaderHeight()
+    }
+
+    public openDialog(item: Article): void
+    {
+        this.dialog.open(DialogComponentComponent, {
+            data: {
+                article: item
+            }
+        });
+    }
+
+    @HostListener('window:resize', ['$event'])
+    public onResize(): void
+    {
+        console.log('onResize');
+        this.refreshHeaderHeight();
+    }
+
+    public refreshHeaderHeight(): void
+    {
+        this.innerHeight = `${window.innerHeight - 210}px`;
+    }
 }
