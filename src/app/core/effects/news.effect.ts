4356ab868065f7ba8f9e4705ca01efe2e45a1468
paullo97 <paulo.cesar97@hotmail.com>
2022-08-30 10:10:46 -0300

    include method in effects to listen action and dispath service to get languages

--- src/app/core/effects/news.effect.ts
+++ src/app/core/effects/news.effect.ts
@@ -2,23 +2,32 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { EMPTY } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
+import { languagesModel } from '../services/models/language.model';
 import { Response } from '../services/models/response.model';
 import { NewsService } from '../services/news.service';
-import { loadNews, loadNewsSuccess } from '../store/news/news.actions';
+import { loadLanguagesSuccess, loadNews, loadNewsSuccess } from '../store/news/news.actions';
+import { loadLanguages } from './../store/news/news.actions';
 
 @Injectable()
 export class NewsEffect
 {
-  getNews$ = createEffect(() => this.actions$.pipe(
-    ofType(loadNews),
-    switchMap((response) => this.service.getNews(response.request)),
-    map((response: Response) => loadNewsSuccess({ articles: response })),
-    catchError(() => EMPTY)
-  ));
+    getNews$ = createEffect(() => this.actions$.pipe(
+        ofType(loadNews),
+        switchMap((response) => this.service.getNews(response.request)),
+        map((articles: Response) => loadNewsSuccess({ articles })),
+        catchError(() => EMPTY)
+    ));
 
-  constructor(
-    private readonly actions$: Actions,
-    private readonly service: NewsService,
-  )
-  { }
+    getLanguages$ = createEffect(() => this.actions$.pipe(
+        ofType(loadLanguages),
+        switchMap(() => this.service.getLanguages()),
+        map((languages: Array<languagesModel>) => loadLanguagesSuccess({ languages })),
+        catchError(() => EMPTY)
+    ));
+
+    constructor(
+        private readonly actions$: Actions,
+        private readonly service: NewsService,
+    )
+    { }
 }
