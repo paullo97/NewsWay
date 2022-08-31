08d465ecd8786443bea39c2993b2f33e07223487
paullo97 <paulo.cesar97@hotmail.com>
2022-08-30 21:35:26 -0300

    create method to open dialog do confirm

--- src/app/components/dialog-component/dialog-component.component.ts
+++ src/app/components/dialog-component/dialog-component.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { ConfirmationDialogComponent } from 'src/app/components/confirmation-dialog/confirmation-dialog.component';
 import { loadFullArticle } from 'src/app/core/store/full-article/article.actions';
 import { FullArticleStore } from 'src/app/core/store/full-article/article.store';
 import { Article } from './../../core/services/models/response.model';
@@ -16,7 +17,8 @@ export class DialogComponentComponent implements OnInit
 
     constructor(
         @Inject(MAT_DIALOG_DATA) public data: any,
-        private readonly store: Store<FullArticleStore>
+        private readonly store: Store<FullArticleStore>,
+        private readonly dialog: MatDialog
     )
     {
         this.article = this.data.article;
@@ -24,9 +26,23 @@ export class DialogComponentComponent implements OnInit
 
     public ngOnInit(): void
     {
-        console.log('passou');
         this.article = this.data.article;
         this.store.dispatch(loadFullArticle({ url: this.article.url }));
+    }
+
+   /**
+    * The function opens a dialog box with a confirmation message and a link to the article
+    */
+    public outside(): void
+    {
+        this.dialog.open(ConfirmationDialogComponent,
+            {
+                disableClose: true,
+                data: {
+                    url: this.article.url
+                }
+            }
+        );
     }
 
 }
